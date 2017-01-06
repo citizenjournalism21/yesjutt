4ad29f4d0eee44110c54352aa3493b1d52461513
Arslan <citizenjournalism21@gmail.com>
2017-01-06 21:21:28 +0500

    bc

--- CJ/settings/production.py
+++ CJ/settings/production.py
@@ -10,7 +10,7 @@ import dj_database_url
 DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['evening-thicket-93628.herokuapp.com']
 
 # import os
 # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
