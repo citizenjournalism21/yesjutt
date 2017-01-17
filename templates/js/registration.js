c4fb12484fae812a4005fc691f04b91ebc4d5790
Monam <citizenjournalism21@gmail.com>
2017-01-17 11:46:15 +0500

    adding my website

---
+++ templates/js/registration.js
@@ -0,0 +1,75 @@
+var Registration_HTML =  
+        '<div class="modal-dialog modal-lg">' + "\n" +
+                '<div class="modal-content">' + "\n" +
+                    '<div class="modal-header">' + "\n" +
+                        '<h4 class="modal-title" id="myModalLabel">Registration</h4>' + "\n" +
+                    '</div>' + "\n" +
+                    '<div class="modal-body">' + "\n" +
+                        '<div class="row">' + "\n" +
+                            '<div class="col-md-8" style="border-right: 1px dotted #C2C2C2;padding-right: 30px;">' + "\n" +
+                                '<div class="tab-pane" id="Registration">' + "\n" +
+                                    '<form role="form" class="form-horizontal">' + "\n" +
+                                    '<div class="form-group">' + "\n" +
+                                        '<label for="email" class="col-sm-2 control-label">Name</label>' + "\n" +
+                                        '<div class="col-sm-10">' + "\n" +
+                                            '<div class="row">' + "\n" +
+                                                '<div class="col-md-3">' + "\n" +
+                                                    '<select class="form-control">' + "\n" +
+                                                        '<option>Mr.</option>' + "\n" +
+                                                        '<option>Ms.</option>' + "\n" +
+                                                        '<option>Mrs.</option>' + "\n" +
+                                                    '</select>' + "\n" +
+                                                '</div>' + "\n" +
+                                                '<div class="col-md-9">' + "\n" +
+                                                    '<input type="text" class="form-control" placeholder="Name">' + "\n" +
+                                                '</div>' + "\n" +
+                                            '</div>' + "\n" +
+                                        '</div>' + "\n" +
+                                    '</div>' + "\n" +
+                                    '<div class="form-group">' + "\n" +
+                                        '<label for="email" class="col-sm-2 control-label">Email</label>' + "\n" +
+                                        '<div class="col-sm-10">' + "\n" +
+                                            '<input type="email" class="form-control" id="email" placeholder="Email">' + "\n" +
+                                        '</div>' + "\n" +
+                                    '</div>' + "\n" +
+                                    '<div class="form-group">' + "\n" +
+                                        '<label for="mobile" class="col-sm-2 control-label">Mobile</label>' + "\n" +
+                                        '<div class="col-sm-10">' + "\n" +
+                                            '<input type="email" class="form-control" id="mobile" placeholder="Mobile">' + "\n" +
+                                        '</div>' + "\n" +
+                                    '</div>' + "\n" +
+                                    '<div class="form-group">' + "\n" +
+                                        '<label for="password" class="col-sm-2 control-label">Password</label>' + "\n" +
+                                        '<div class="col-sm-10">' + "\n" +
+                                            '<input type="password" class="form-control" id="password" placeholder="Password">' + "\n" +
+                                        '</div>' + "\n" +
+                                    '</div>' + "\n" +
+                                    '<div class="row">' + "\n" +
+                                        '<div class="col-sm-2">' + "\n" +
+                                        '</div>' + "\n" +
+                                        '<div class="col-sm-10">' + "\n" +
+                                            '<button type="button" class="btn btn-primary btn-sm">Save &amp; Continue</button>' + "\n" +
+                                        '</div>' + "\n" +
+                                    '</div>' + "\n" +
+                                    '</form>' + "\n" +
+                                '</div>' + "\n" +
+                                '<div id="OR" class="hidden-xs">OR</div>' + "\n" +
+                            '</div>' + "\n" +
+                            '<div class="col-md-4">' + "\n" +
+                                '<div class="row text-center sign-with">' + "\n" +
+                                    '<div class="col-md-12">' + "\n" +
+                                        '<h3>Sign in with</h3>' + "\n" +
+                                    '</div>' + "\n" +
+                                    '<div class="col-md-12">' + "\n" +
+                                        '<div class="btn-group btn-group-justified">' + "\n" +
+                                            '<a href="http://bootsnipp-env.elasticbeanstalk.com/iframe/W7Xmz#" class="btn btn-primary">Facebook</a>' + "\n" + 
+                                            '<a href="http://bootsnipp-env.elasticbeanstalk.com/iframe/W7Xmz#" class="btn btn-danger">Google</a>' + "\n" +
+                                        '</div>' + "\n" +
+                                    '</div>' + "\n" +
+                                '</div>' + "\n" +
+                            '</div>' + "\n" +
+                        '</div>' + "\n" +
+                    '</div>' + "\n" +
+                '</div>' + "\n" +
+            '</div>' + "\n" +
+        '</div>';
